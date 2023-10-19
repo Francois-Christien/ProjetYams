829d897ea943175dfe0e195b731e73013337c6a2
Arnaud-M07 <arnaudmonteux@gmail.com>
2023-10-19 15:49:45 +0200

    Push branch functionCalcPoints (création de la fonction)

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -50,6 +50,19 @@
 // // // // 4.Fonction Calcul des points // // // // // //
 // // // // // // // // // // // // // // // // // // // //
 
+function calcPoints(){
+    // Tableau récupérant la valeur de chaque opération
+    const tableauScore = [total1, total2, total3, total4, total5, total6, bonus, brelan, carre, full, petiteSuite, grandeSuite, yams, chance];
+    let sumPoints = 0;
+    // Additionner les élements tu tableau de score
+    for (let point = 0; point<tableauScore.length; point++){
+        sumPoints += tableauScore[point];
+    }
+    console.log(sumPoints)
+}
+// Appel de la fonction
+calcPoints();
+
 
 // // // // // // // // // // // // // // // // // // // //
 // // // // 4.Fonction Calcul des points // // // // // //
