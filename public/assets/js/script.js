35b5488c90dfb06537820c7f85f9aa08d018658c
Arnaud-M07 <arnaudmonteux@gmail.com>
2023-10-20 10:25:37 +0200

    Affectation visuelle des valeurs de "rollDices"

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,24 +1,34 @@
 // // // // // // // // // // // // // // // // // // // //
 // // // // // // 1.Fonction Lancer les dés // // // // //
 // // // // // // // // // // // // // // // // // // // //
-// test
+// Récupération des élements Dés
+let dices = document.querySelectorAll('.diceButton');
+// Récupération du bouton "Lancer les dés"
+let rollDicesButton = document.getElementById('throwDice');
 
-// // // // // // // // // // // // // // // // // // // //
-// // // // // // 1.Fin fonction Lancer les dés // // // //
-// // // // // // // // // // // // // // // // // // // //
-
-
+// Fonction Lancer de dés - Jing
 const numberOfTheTrials = 3;
 let dicesValues = [];
 
 function rollDices(numberOfTheTotalDices) {
-    for (let i = 0; i < numberOfTheDice; i++) {
+    for (let i = 0; i < numberOfTheTotalDices; i++) {
         dicesValues[i] = Math.floor(Math.random() * 6 + 1);
-    }
-    console.log(dicesValues);
-}
-
+    };
+};
 rollDices(5);
+
+// Affecter visuellement aux dés les résultats du lancer 
+for(index=0;index<dices.length;index++){
+    dices[index].innerHTML = dicesValues[index];
+}
+// Event Listener 
+rollDicesButton.addEventListener("click", rollDices)
+
+
+
+// // // // // // // // // // // // // // // // // // // //
+// // // // // // 1.Fin fonction Lancer les dés // // // //
+// // // // // // // // // // // // // // // // // // // //
 
 
 // // // // // // // // // // // // // // // // // // // //
