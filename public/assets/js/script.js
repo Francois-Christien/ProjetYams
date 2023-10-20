7098fe20ea879084421fbc2984dfb352b735506a
Jing ZHANG <jing.zhang.ferment@gmail.com>
2023-10-20 10:31:22 +0200

    function blockDices

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,29 +1,55 @@
 // // // // // // // // // // // // // // // // // // // //
 // // // // // // 1.Fonction Lancer les dés // // // // //
 // // // // // // // // // // // // // // // // // // // //
-// test
+function rollDices(numberOfTheTotalDices) {
+    let dicesValues = [];
+
+    for (let i = 0; i < numberOfTheTotalDices; i++) {
+        dicesValues[i] = Math.floor(Math.random() * 6 + 1);
+    }
+    return dicesValues;
+}
+console.log(rollDices(5));
 
 // // // // // // // // // // // // // // // // // // // //
 // // // // // // 1.Fin fonction Lancer les dés // // // //
 // // // // // // // // // // // // // // // // // // // //
 
-
-
-
 // // // // // // // // // // // // // // // // // // // //
 // // // // // // 2.Fonction Bloquer les dés // // // // //
 // // // // // // // // // // // // // // // // // // // //
 
+function restartDices() {
+    // let resultsFirstRoll= rollDices(5);
+
+    let keepDices = [];
+
+    // choix utilisateur 
+    keepDices.push(4,5);
+
+    console.log(keepDices);
+    
+    let resultsSecondRoll = rollDices(3);
+    
+    console.log(resultsSecondRoll)
+
+    resultsSecondRoll.forEach((dice)=> {
+      // choix utilisateur 
+        console.log(keepDices.push(dice));
+    })
+    console.log(keepDices);
+
+}
+
+restartDices();
 
 // // // // // // // // // // // // // // // // // // // //
 // // // // // // 2.Fin Fonction Bloquer les dés// // // //
 // // // // // // // // // // // // // // // // // // // //
 
 
-
-
 // // // // // // // // // // // // // // // // // // // //
-// // // 3.Fonctions pour chaque combinaison possible // // 
+// // // 3.Fonctions pour chaque combinaison possible // //
 // // // // // // // // // // // // // // // // // // // //
 
 // total1
@@ -36,6 +62,29 @@
 // brelan
 // carré
 // full
+
+// let dicesValues = [2 ,2 ,2 ,3 ,3]
+// // déterminer le nombres d'occurence d'une valeur
+
+// const nbrOfoccurrences = (value) => {
+//     let nbr = 0
+//     dicesValues.forEach((dicesValue, i) => {
+//         if (value == dicesValue){
+//             nbr++
+//         }
+
+//     })
+//     return nbr
+// };
+
+// console.log(nbrOfoccurrences(1));
+// console.log(nbrOfoccurrences(2));
+// console.log(nbrOfoccurrences(3));
+// console.log(nbrOfoccurrences(4));
+// console.log(nbrOfoccurrences(5));
+// console.log(nbrOfoccurrences(6));
+
+
 // petiteSuite
 // grandeSuite
 // yams
